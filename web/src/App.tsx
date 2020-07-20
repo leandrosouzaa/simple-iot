d89ac93ee3d1b6f5f28055c222c8831f2fd77b41
leandrosouzaa <leandror.dev@gmail.com>
2020-07-20 13:03:25 -0300

    strcuture and styles done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import './App.css'
+
 function App() {
   return (
     <div className="main">
-      Ola Mundo
-    </div>
-  );
-}
+      <div className="content">
+        <h1>Simple IoT</h1>
 
-export default App;
+        <h3>Button Control</h3>
+        <div className="row">
+          <button>Turn On</button>
+          <button>Turn Off</button>
+        </div>
+
+        <h3>Write in LCD display</h3>
+        <input placeholder="Max 16 characters" maxLength={16}/>
+        <button style={{marginTop: 8}}>Send</button>
+
+        <h3>Blink Led</h3>
+        <div className="row">
+          <input style={{width: '70%'}} placeholder="Interval (in seconds)" maxLength={16}/>
+          <button style={{width: '28%'}}>Blink</button>
+        </div>
+      </div>
+    </div>
+    );
+  }
+  
+  export default App;
+  
